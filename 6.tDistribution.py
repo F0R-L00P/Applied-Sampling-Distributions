bee589b305755b90463c588bd5263cb6127c650b
F0R-L00P <67719682+F0R-L00P@users.noreply.github.com>
2024-01-16 03:24:34 -0700

    20240116

---
+++ 6.tDistribution.py
@@ -0,0 +1,11 @@
+'''
+The t-distribution is a normally shaped distribution, 
+except that it is a bit thicker and longer on the tails.
+
+n
+    Sample size.
+
+Degrees of freedom
+    A parameter that allows the t-distribution to adjust to 
+    different sample sizes, statistics, and numbers of groups.
+'''
